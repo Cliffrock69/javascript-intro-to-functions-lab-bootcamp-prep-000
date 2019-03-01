6208fb0f66aeb02d4b32c7415c06c07bd018ac32
Cliff Conolly <cliffconolly@protonmail.com>
2019-03-01 17:27:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ function logWhisper(string) {
 function 'sayHiToGrandma(string) {
   return ("I can't hear you") ===.toLowerCase()
 }
+
+describe('shout(string)', function() {
+  it('receives one argument and returns it in all caps', function() {
+    expect(shout('hello')).toEqual('HELLO')
+  })
+})
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
