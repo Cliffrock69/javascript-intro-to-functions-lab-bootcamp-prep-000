b7f99ee0c68388bbd958f21d26b2ec5dd1363f3c
Cliff Conolly <cliffconolly@protonmail.com>
2019-03-01 19:24:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,4 +30,13 @@ function sayHiToGrandma(string) {
   return ("I can't hear you!");
 }
 
-    
+{
+if (string === toUpperCase)
+return ("YES INDEED!");
+}
+
+  {
+  if (string === "I love you, Grandma.")
+  return ("I love you, too.");
+  }
+}  
